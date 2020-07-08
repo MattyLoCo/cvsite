31593c0cb31f342bea76ea0e6b8f88c62f53fd7e
Matthew Lopez <lopezmatt6891@gmail.com>
2020-07-08 11:13:12 -0600

    chore: Create page modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { ExtrasComponent } from './extras/extras.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ExtrasComponent,
+    FooterComponent,
+    ContactComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
